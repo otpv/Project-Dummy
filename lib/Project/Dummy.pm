8205cd53690f2c53b0141427bdb1a91aa70773ef
Olivier VALUET <olivier@olivier-VirtualBox.(none)>
2010-12-03 22:11:32 -0500

    initial commit

---
+++ lib/Project/Dummy.pm
@@ -0,0 +1,34 @@
+package Project::Dummy;
+BEGIN {
+    $__PACKAGE__::VERSION   = '0.001001';
+    $__PACKAGE__::AUTHORITY = 'cpan:oval';
+}
+# ABSTRACT: a really awesome library
+
+use 5.012;
+use utf8;
+use strict;
+use warnings;
+
+1;
+
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+...
+
+=method method_x
+
+This method does something experimental.
+
+=method method_y
+
+This method returns a reason.
+
+=head1 SEE ALSO
+
+=for :list
+* L<Project::Dummy>
